6e2b067e5c11dac2e87ec1c5bdf813f803979959
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2021-02-22 19:32:05 -0300

    Aula 1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ExperienceBar } from './components/ExperienceBar';
+
+import "./styles/global.css"
+
+export default function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
